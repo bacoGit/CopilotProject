242ded83caeffe5ab2c3b2dfbb9c18584e560944
Oscar Badillo <bacoGit@users.noreply.github.com>
2025-01-13 20:40:23 -0600

    Initial Commit

---
+++ script.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', (event) => {
+    const button = document.getElementById('myButton');
+    button.addEventListener('click', () => {
+        alert('Button was clicked!');
+    });
+});
