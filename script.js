b3ba2932e5163bf022a939192e21eb2badec8ff2
osbadillo <osbadillo2778@gmail.com>
2025-01-15 09:02:43 -0600

    Add more interaction

--- script.js
+++ script.js
@@ -1,6 +1,19 @@
-document.addEventListener('DOMContentLoaded', (event) => {
-    const button = document.getElementById('myButton');
-    button.addEventListener('click', () => {
-        alert('Button was clicked!');
-    });
-});
+function linkClicked(text) {
+    switch (text) {
+        case 'home':
+            document.getElementById('home').style.display = 'block';
+            document.getElementById('about').style.display = 'none';
+            document.getElementById('contact').style.display = 'none';
+            break;
+        case 'about':
+            document.getElementById('home').style.display = 'none';
+            document.getElementById('about').style.display = 'block';
+            document.getElementById('contact').style.display = 'none';
+            break;
+        case 'contact':
+            document.getElementById('home').style.display = 'none';
+            document.getElementById('about').style.display = 'none';
+            document.getElementById('contact').style.display = 'block'; 
+            break;
+    }
+}
